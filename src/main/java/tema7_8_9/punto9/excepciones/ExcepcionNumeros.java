75b09a61e3ad6793f71165ea306e744a2727dcd8
Escanor <grcc9a@gmail.com>
2023-03-09 00:09:06 -0500

    Se suben los ejercicios de los temas 7, 8 y 9

---
+++ src/main/java/tema7_8_9/punto9/excepciones/ExcepcionNumeros.java
@@ -0,0 +1,8 @@
+package tema7_8_9.punto9.excepciones;
+
+public class ExcepcionNumeros extends Exception{
+
+    public ExcepcionNumeros(){
+        super("Solo se permiten valores positivos que sean enteros");
+    }
+}
